f22dcf24c7c197ab07a6b43d5a654d57db2ebe2b
Ben McCormick <mr.mccormick@Bens-MacBook-Pro.local>
2024-01-23 20:33:33 -0800

    Added queries and mutations to server

--- server/schema.js
+++ server/schema.js
@@ -35,3 +35,50 @@ module.exports = new GraphQLSchema({
   query: RootQuery,
   mutation: Mutation,
 });
+// ... (previous code)
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      user: {
+        type: UserType,
+        args: { id: { type: GraphQLID } },
+        resolve(parent, args) {
+          return User.findById(args.id);
+        },
+      },
+      products: {
+        type: new GraphQLList(ProductType),
+        resolve() {
+          return Product.find();
+        },
+      },
+      // Add more queries as needed
+    },
+  });
+  
+  const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+      addUser: {
+        type: UserType,
+        args: { name: { type: GraphQLString }, email: { type: GraphQLString }, password: { type: GraphQLString } },
+        resolve(parent, args) {
+          // Logic to add a new user
+          return User.create(args);
+        },
+      },
+      addProduct: {
+        type: ProductType,
+        args: { name: { type: GraphQLString }, description: { type: GraphQLString }, price: { type: GraphQLFloat } },
+        resolve(parent, args) {
+          // Logic to add a new product
+          return Product.create(args);
+        },
+      },
+      // Add more mutations as needed
+    },
+  });
+  
+  // ... (remaining code)
+  
