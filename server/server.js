a67f6cde3270ef4883bd34465ef36f50330820dd
Ben McCormick <mr.mccormick@Bens-MacBook-Pro.local>
2024-01-29 15:55:18 -0800

    Fixed MongoDB URI Connection

--- server/server.js
+++ server/server.js
@@ -10,7 +10,8 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://mccormickben45:LUGcWvgVv1oZ1GdB@nexusmarketpost.rgrym05.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 app.use(express.json());
 
