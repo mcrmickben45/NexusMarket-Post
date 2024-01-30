0f4d4a735ac1579230d6f590cdbb452261e4fc4d
aqtagon <ahmed.danawi@gmail.com>
2024-01-29 19:58:41 -0800

    Made changes to monggoDB

--- server/mongoDB.js
+++ server/mongoDB.js
@@ -1,9 +1,8 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
 // Replace <password> with your actual password
 const password = process.env.MONGO_PASSWORD;
 const uri = `mongodb+srv://mccormickben45:${password}@nexusmarketpost.rgrym05.mongodb.net/`;
-
+const mongodbUriCollaborator1 = 'mongodb+srv://aqtagon:LUGcWvgVv1oZ1GdB@atlascluster.ciopdlq.mongodb.net/?retryWrites=true&w=majority';
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -12,7 +11,16 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
-
+// Choose the MongoDB URI based on some condition or configuration
+let chosenMongoDBUri;
+if (/* some condition */) {
+    chosenMongoDBUri = mongodbUriCollaborator1;
+} else if (/* some other condition */) {
+    chosenMongoDBUri = mongodbUriCollaborator2;
+} else {
+    chosenMongoDBUri = mongodbUri; // Use the default URI
+}
+// Use chosenMongoDBUri in your MongoDB connection logic
 async function run() {
   try {
     // Connect the client to the server (optional starting in v4.7)
@@ -25,7 +33,5 @@ async function run() {
     await client.close();
   }
 }
-
 run().catch(console.dir);
-
 module.exports = client;
